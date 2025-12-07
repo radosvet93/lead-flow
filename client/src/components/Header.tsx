c532c0aa76829869ac981c42cd8c9869956afb00
Radosvet Petrov <93radosvet@gmail.com>
2025-12-07 15:24:18 +0000

    added link to h1

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,9 @@ const Header = ({ hasBackButton = false }: { hasBackButton?: boolean }) => {
   return (
     <header className="bg-primary text-primary-foreground p-4">
       <div className="flex justify-between">
-        <h1>Lead Flow</h1>
+        <Link to="/">
+          <h1>Lead Flow</h1>
+        </Link>
         {hasBackButton && (
           <Link to="/">
             <Button className="bg-primary-foreground text-primary hover:bg-primary-foreground hover:opacity-90">
